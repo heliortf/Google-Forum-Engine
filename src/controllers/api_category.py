ffc1d938aa8aa978eefe73e928bec9afe611107f
Helio Ricardo <heliortf@gmail.com>
2017-02-21 23:52:17 -0300

    Adicionado swagger yaml

--- src/controllers/api_category.py
+++ src/controllers/api_category.py
@@ -1,10 +1,16 @@
 
 from src import app
 
-@app.route('/api/v1/categories')
+@app.route('/api/v1/categories', methods=['GET'])
 def categories():
     return "categories!"
 
-@app.route('/api/v1/categories/<id>')
+
+@app.route('/api/v1/categories/<id>', methods=['GET'])
 def category(id):
+    """
+        Return a category from its id
+    """
+    
     return "category! "+id
+
