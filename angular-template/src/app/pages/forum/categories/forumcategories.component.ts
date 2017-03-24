00a806734eb2a6ee43992d418bedf3a919825d1b
= <heliortf@gmail.com>
2017-03-24 12:14:27 -0300

    Beginning  of the categories page

---
+++ angular-template/src/app/pages/forum/categories/forumcategories.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+
+// This component buidls the layout around the forum and show its pages inside
+
+@Component({
+  selector: 'pages',
+  templateUrl: 'forum.html'
+})
+export class ForumCategories {
+    constructor(){
+
+    }
+}
